405eda49a43d6f31d962eb32dda504b7950e2eb8
c-c-k <c-c-k@nym.hush.com>
2023-04-27 16:33:21 +0300

    stage 1 done

--- calculator.sh
+++ calculator.sh
@@ -1,2 +1,22 @@
 #!/usr/bin/env bash
 
+# helper functions
+function is_int { [[ "$1" =~ ^-?[0-9]+$ ]] ; }
+
+function is_operator { [[ "$1" =~ ^[-+*/]$ ]] ; }
+
+function is_valid_input { (( "$#" == 3 )) && is_int "$1" && is_operator "$2" && is_int "$3" ; }
+
+
+# get user input
+echo "Enter an arithmetic operation:"
+read -a USER_INPUT -r
+
+
+# test user input
+if is_valid_input "${USER_INPUT[@]}"
+then
+    echo 'Operation check passed!'
+else
+    echo 'Operation check failed!'
+fi

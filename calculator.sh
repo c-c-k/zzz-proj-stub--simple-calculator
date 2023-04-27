b4a285f1e66817bd9b644e62619b277ee00fd199
c-c-k <c-c-k@nym.hush.com>
2023-04-27 16:33:21 +0300

    stage 3 done

--- calculator.sh
+++ calculator.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # helper functions
-function is_int { [[ "$1" =~ ^-?[0-9]+$ ]] ; }
+function is_int { [[ "$1" =~ ^-?[0-9]+\.?[0-9]*$ ]] ; }
 
-function is_operator { [[ "$1" =~ ^[-+*/]$ ]] ; }
+function is_operator { [[ "$1" =~ ^[-+*/^]$ ]] ; }
 
 function is_valid_input { (( "$#" == 3 )) && is_int "$1" && is_operator "$2" && is_int "$3" ; }
 
-function calculate { echo $(( "$1" "$2" "$3" )) ; }
+function calculate { echo "scale=2; $1 $2 $3"|bc --mathlib ; }
 
 # get user input
 echo "Enter an arithmetic operation:"

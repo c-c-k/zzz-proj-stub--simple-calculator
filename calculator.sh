54c289aa2969298abbf811f9e117f061e91749b7
c-c-k <c-c-k@nym.hush.com>
2023-04-27 16:33:21 +0300

    stage 4 done

--- calculator.sh
+++ calculator.sh
@@ -9,15 +9,27 @@ function is_valid_input { (( "$#" == 3 )) && is_int "$1" && is_operator "$2" && 
 
 function calculate { echo "scale=2; $1 $2 $3"|bc --mathlib ; }
 
-# get user input
-echo "Enter an arithmetic operation:"
-read -a USER_INPUT -r
+# initialize history file
+HISTORY="operation_history.txt"
+rm "$HISTORY" 2>/dev/null
+
+echo "Welcome to the basic calculator!" | tee -a  "$HISTORY"
+while true
+do
+    # get user input
+    echo "Enter an arithmetic operation or type 'quit' to quit:" | tee -a  "$HISTORY"
+    read -a USER_INPUT -r
+    echo "${USER_INPUT[@]}" >>"$HISTORY"
+    [[ "${USER_INPUT[0]}" == "quit" ]] && break
 
 
-# test user input
-if is_valid_input "${USER_INPUT[@]}"
-then
-    calculate "${USER_INPUT[@]}"
-else
-    echo 'Operation check failed!'
-fi
+    # test user input
+    if is_valid_input "${USER_INPUT[@]}"
+    then
+        calculate "${USER_INPUT[@]}" | tee -a  "$HISTORY"
+    else
+        echo 'Operation check failed!' | tee -a  "$HISTORY"
+    fi
+done
+echo "Goodbye!" | tee -a  "$HISTORY"
+
